f611d0104e153712062223055c674bb4c5cdc281
Jude Parfait NGOM NZE <jud10parfait@gmail.com>
2020-06-30 13:47:42 +0100

    Layouts & Partials des Mail custom templates

--- config/mail.php
+++ config/mail.php
@@ -113,7 +113,7 @@ return [
     */
 
     'markdown' => [
-        'theme' => 'default',
+        'theme' => 'mailstyle01',
 
         'paths' => [
             resource_path('views/vendor/mail'),
--- resources/views/emails/affectations/new.blade.php
+++ resources/views/emails/affectations/new.blade.php
@@ -1,27 +1,103 @@
-@component('mail::message')
-    # Order Shipped
+@extends('emails.layouts.master')
 
-    Your order has been shipped!
+@section('content')
 
-    @component('mail::button', ['url' => $url, 'color' => 'primary'])
-        View Order
-    @endcomponent
+    <tr>
+        <td class="body" width="100%" cellpadding="0" cellspacing="0">
+            <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0">
+                <!-- Body content --><tr>
+                    <td class="content-cell">
+                        <h1>Heading 1</h1>
+                        <p>This is a paragraph filled with Lorem Ipsum and a link. Cumque dicta <a href="">doloremque eaque</a>, enim error laboriosam pariatur possimus tenetur veritatis voluptas.</p>
+                        <h2>Heading 2</h2>
+                        <div class="table">
+                            <table>
+                                <thead><tr>
+                                    <th>Product</th>
+                                    <th>Description</th>
+                                    <th>Price</th>
+                                </tr></thead>
+                                <tbody>
+                                <tr>
+                                    <td>Product 1</td>
+                                    <td>Lorem Ipsum</td>
+                                    <td>$10</td>
+                                </tr>
+                                <tr>
+                                    <td>Product 2</td>
+                                    <td>Lorem ipsum dolor sit amet.</td>
+                                    <td>$20</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <h3>Heading 3</h3>
+                        <p >This is a paragraph filled with Lorem Ipsum and a link. Cumque dicta <a href="">doloremque eaque</a>, enim error laboriosam pariatur possimus tenetur veritatis voluptas.</p>
+                        <table class="action" align="center" width="100%" cellpadding="0" cellspacing="0"><tr>
+                                <td align="center">
+                                    <table width="100%" border="0" cellpadding="0" cellspacing="0"><tr>
+                                            <td align="center">
+                                                <table border="0" cellpadding="0" cellspacing="0"><tr>
+                                                        <td>
+                                                            <a href="#" class="button button-blue" target="_blank">Blue button</a>
+                                                        </td>
+                                                    </tr></table>
+                                            </td>
+                                        </tr></table>
+                                </td>
+                            </tr></table>
+                        <table class="action" align="center" width="100%" cellpadding="0" cellspacing="0"><tr>
+                                <td align="center">
+                                    <table width="100%" border="0" cellpadding="0" cellspacing="0"><tr>
+                                            <td align="center">
+                                                <table border="0" cellpadding="0" cellspacing="0"><tr>
+                                                        <td>
+                                                            <a href="#" class="button button-green" target="_blank">Green button</a>
+                                                        </td>
+                                                    </tr></table>
+                                            </td>
+                                        </tr></table>
+                                </td>
+                            </tr></table>
+                        <table class="action" align="center" width="100%" cellpadding="0" cellspacing="0"><tr>
+                                <td align="center">
+                                    <table width="100%" border="0" cellpadding="0" cellspacing="0"><tr>
+                                            <td align="center">
+                                                <table border="0" cellpadding="0" cellspacing="0"><tr>
+                                                        <td>
+                                                            <a href="#" class="button button-red" target="_blank">Red button</a>
+                                                        </td>
+                                                    </tr></table>
+                                            </td>
+                                        </tr></table>
+                                </td>
+                            </tr></table>
+                        <table class="panel" width="100%" cellpadding="0" cellspacing="0"><tr>
+                                <td class="panel-content">
+                                    <table width="100%" cellpadding="0" cellspacing="0"><tr>
+                                            <td class="panel-item">
+                                                <p>How awesome is this panel?</p>
+                                            </td>
+                                        </tr></table>
+                                </td>
+                            </tr></table>
+                        <p >Heading 3</p>
+                        <table class="promotion" align="center" width="100%" cellpadding="0" cellspacing="0"><tr>
+                                <td align="center">
+                                    <p>Coupon code: MarkdownMail</p>
+                                </td>
+                            </tr></table>
+                        <p >Thanks,<br>
+                            MarkdownMail</p>
+                        <table class="subcopy" width="100%" cellpadding="0" cellspacing="0"><tr>
+                                <td>
+                                    <p>This is the subcopy of the email</p>
+                                </td>
+                            </tr></table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
 
-    @component('mail::button', ['url' => $url, 'color' => 'success'])
-        View Order
-    @endcomponent
-
-    @component('mail::panel')
-        This is the panel content.
-    @endcomponent
-
-    @component('mail::table')
-    | Laravel       | Table         | Example  |
-    | ------------- |:-------------:| --------:|
-    | Col 2 is      | Centered      | $10      |
-    | Col 3 is      | Right-Aligned | $20      |
-    @endcomponent
-
-    Thanks,<br>
-    {{ config('app.name') }}
-@endcomponent
+@endsection
