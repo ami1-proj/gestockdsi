050689be36790220a268f0b604b474438d5ca3d4
Jude Parfait NGOM NZE <jud10parfait@gmail.com>
2020-06-30 18:24:29 +0100

    Mail: finilisation du template

--- resources/views/emails/affectations/new.blade.php
+++ resources/views/emails/affectations/new.blade.php
@@ -5,21 +5,19 @@
     Bonjour.
 </p>
 <p>
-    Une nouvelle affectation de Matériels informatique a été programmé pour vous
+    Une nouvelle affectation de Matériels informatique a été programmée pour vous.
 </p>
 <p>
-    Objet: <strong>{{ $affectation->objet  }}</strong>
+    <u>Objet:</u> <strong>{{ $affectation->objet  }}</strong>
 </p>
 
 ## Liste de Matériels
-@component('mail::panel')
 @component('mail::table')
     | Type      | Référence     | Marque    |
     |:--------- |:------------- |:--------- |
     @foreach($articles as $atc)
         | {{$atc->typeArticle->libelle}}     | {{$atc->reference}} |        {{$atc->marqueArticle->nom}} |
     @endforeach
-@endcomponent
 @endcomponent
 
 @component('mail::button', ['url' => ''])
