f481b72f1cd867759c0b15e0965f2284a57ad3ba
Jude Parfait NGOM NZE <jud10parfait@gmail.com>
2020-06-30 18:12:37 +0100

    Layouts & Partials des Mail custom templates

--- resources/views/emails/affectations/new.blade.php
+++ resources/views/emails/affectations/new.blade.php
@@ -12,6 +12,7 @@
 </p>
 
 ## Liste de Matériels
+@component('mail::panel')
 @component('mail::table')
     | Type      | Référence     | Marque    |
     |:--------- |:------------- |:--------- |
@@ -19,15 +20,12 @@
         | {{$atc->typeArticle->libelle}}     | {{$atc->reference}} |        {{$atc->marqueArticle->nom}} |
     @endforeach
 @endcomponent
-
-@component('mail::panel')
-    The information contained in this website is for general information purposes only. The information is provided by Gamesstation, while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind. Any reliance you place on such information is strictly at your own risk.
 @endcomponent
 
 @component('mail::button', ['url' => ''])
 Button Text
 @endcomponent
 
-Thanks,<br>
+Cordialement,<br>
 {{ config('app.name') }}
 @endcomponent
